4a258b02a5a7412d01c06c90344d1b2f582235dc
Scott Nelson <scott@scttnlsn.com>
2013-05-18 22:46:04 -0400

    Added queue

---
+++ kew/queue.go
@@ -0,0 +1,72 @@
+package kew
+
+import (
+	"time"
+)
+
+var Queues = map[string]*Queue{}
+
+const NilDuration = time.Duration(-1)
+
+type Queue struct {
+	Name  string
+	Items chan *Item
+}
+
+func NewQueue(name string) *Queue {
+	q, ok := Queues[name]
+	if !ok {
+		q = &Queue{name, make(chan *Item)}
+		Queues[name] = q
+	}
+	return q
+}
+
+func (q *Queue) Enqueue(item *Item) {
+	item.dequeued = false
+	item.queue = q.Name
+	q.Items <- item
+}
+
+func (q *Queue) Dequeue(wait time.Duration, timeout time.Duration) (*Item, bool) {
+	if wait != NilDuration {
+		// Blocking
+		expired := time.After(wait)
+		select {
+		case <-expired:
+			return nil, false
+		case item := <-q.Items:
+			item.dequeued = true
+			go q.SetTimeout(item, timeout)
+			return item, true
+		}
+	} else {
+		// Nonblocking
+		select {
+		case item := <-q.Items:
+			item.dequeued = true
+			go q.SetTimeout(item, timeout)
+			return item, true
+		default:
+			return nil, false
+		}
+	}
+
+	return nil, false
+}
+
+func (q *Queue) SetTimeout(item *Item, timeout time.Duration) {
+	if timeout == NilDuration {
+		// Default 30s timeout
+		timeout = time.Second * 30
+	}
+
+	expired := time.After(timeout)
+
+	select {
+	case <-expired:
+		q.Enqueue(item)
+	case <-item.complete:
+		return
+	}
+}
---
+++ kew/queue_test.go
@@ -0,0 +1,59 @@
+package kew
+
+import (
+	"github.com/bmizerany/assert"
+	"testing"
+	"time"
+)
+
+func TestNewQueue(t *testing.T) {
+	q := NewQueue("q1")
+	assert.Equal(t, Queues["q1"], q)
+	assert.Equal(t, NewQueue("q1"), q)
+}
+
+func TestEnqueue(t *testing.T) {
+	q := NewQueue("q2")
+	item := NewItem("foo")
+
+	go q.Enqueue(item)
+	<-q.Items
+
+	assert.Equal(t, item.dequeued, false)
+	assert.Equal(t, item.queue, "q2")
+}
+
+func TestDequeueNonBlocking(t *testing.T) {
+	q := NewQueue("q3")
+
+	_, ok := q.Dequeue(NilDuration, NilDuration)
+	assert.Equal(t, ok, false)
+
+	go q.Enqueue(NewItem("foo"))
+	time.Sleep(time.Millisecond)
+
+	item, ok := q.Dequeue(NilDuration, NilDuration)
+	assert.Equal(t, item.value, "foo")
+	assert.Equal(t, ok, true)
+}
+
+func TestDequeueBlocking(t *testing.T) {
+	q := NewQueue("q4")
+	go q.Enqueue(NewItem("foo"))
+
+	item, ok := q.Dequeue(time.Hour, NilDuration)
+	assert.Equal(t, item.value, "foo")
+	assert.Equal(t, ok, true)
+}
+
+func TestDequeueTimeout(t *testing.T) {
+	q := NewQueue("q5")
+	item := NewItem("foo")
+
+	go q.Enqueue(item)
+	q.Dequeue(time.Hour, time.Millisecond)
+
+	assert.Equal(t, item.dequeued, true)
+	time.Sleep(time.Millisecond * 2)
+	assert.Equal(t, item.dequeued, false)
+}
