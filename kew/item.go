70a1a392133e3cb8670644f40178495e1e88c7ed
Scott Nelson <scott@scttnlsn.com>
2013-05-18 22:45:45 -0400

    Initial commit

---
+++ kew/item.go
@@ -0,0 +1,26 @@
+package kew
+
+type Item struct {
+	id       int
+	value    string
+	queue    string
+	dequeued bool
+	complete chan bool
+}
+
+func NewItem(value string) *Item {
+	complete := make(chan bool)
+	item := &Item{value: value, complete: complete}
+	return item
+}
+
+func (i *Item) Complete() bool {
+	ok := false
+
+	if i.dequeued {
+		i.complete <- true
+		ok = true
+	}
+
+	return ok
+}
