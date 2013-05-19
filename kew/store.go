062a07091e6ef8845546283b73f6e0268f066cdc
Scott Nelson <scott@scttnlsn.com>
2013-05-18 22:46:17 -0400

    Added store

---
+++ kew/store.go
@@ -0,0 +1,147 @@
+package kew
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jmhodges/levigo"
+	"os"
+	"strconv"
+	"sync"
+)
+
+type Store struct {
+	path  string
+	db    *levigo.DB
+	items map[int]*Item
+	mutex sync.Mutex
+	id    int
+}
+
+func NewStore(path string) *Store {
+	opts := levigo.NewOptions()
+	opts.SetCreateIfMissing(true)
+
+	db, err := levigo.Open(path, opts)
+	if err != nil {
+		panic(fmt.Sprintf("kew.Store: Unable to open db: %v", err))
+	}
+
+	store := &Store{path: path, db: db, items: map[int]*Item{}}
+
+	return store
+}
+
+func (s *Store) LastId() int {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	it := s.db.NewIterator(levigo.NewReadOptions())
+	defer it.Close()
+
+	it.SeekToLast()
+
+	id := 0
+
+	if it.Valid() {
+		key, err := strconv.Atoi(string(it.Key()))
+		if err != nil {
+			panic(fmt.Sprintf("kew.Store: Error parsing last id from db: %v", err))
+		}
+		id = key
+	}
+
+	return id
+}
+
+func (s *Store) Get(id int) (*Item, bool) {
+	item, ok := s.items[id]
+	return item, ok
+}
+
+func (s *Store) Put(item *Item) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	item.id = s.id + 1
+
+	key := []byte(fmt.Sprintf("%d", item.id))
+
+	data := map[string]string{
+		"value": item.value,
+		"queue": item.queue,
+	}
+
+	bytes, err := json.Marshal(data)
+
+	if err != nil {
+		panic(fmt.Sprintf("kew.Store: Error marshalling item: %v", err))
+	}
+
+	wopts := levigo.NewWriteOptions()
+	wopts.SetSync(true)
+	s.db.Put(wopts, key, bytes)
+
+	s.items[item.id] = item
+	s.id += 1
+}
+
+func (s *Store) Remove(id int) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	key := []byte(fmt.Sprintf("%d", id))
+
+	wopts := levigo.NewWriteOptions()
+	wopts.SetSync(true)
+	s.db.Delete(wopts, key)
+
+	delete(s.items, id)
+}
+
+func (s *Store) Close() {
+	s.db.Close()
+}
+
+func (s *Store) Drop() {
+	s.Close()
+
+	err := os.RemoveAll(s.path)
+	if err != nil {
+		panic(fmt.Sprintf("kew.Store: Error dropping db: %v", err))
+	}
+}
+
+func (s *Store) Load() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	it := s.db.NewIterator(levigo.NewReadOptions())
+	defer it.Close()
+
+	it.SeekToFirst()
+
+	for it.Valid() {
+
+		id, err := strconv.Atoi(string(it.Key()))
+		if err != nil {
+			panic(fmt.Sprintf("kew: Error loading db: %v", err))
+		}
+
+		data := make(map[string]string)
+
+		err = json.Unmarshal(it.Value(), &data)
+		if err != nil {
+			panic(fmt.Sprintf("kew: Error loading db: %v", err))
+		}
+
+		item := NewItem(data["value"])
+		item.id = id
+
+		q := NewQueue(data["queue"])
+		go q.Enqueue(item)
+
+		s.items[id] = item
+		s.id = id
+		it.Next()
+	}
+}
---
+++ kew/store_test.go
@@ -0,0 +1,73 @@
+package kew
+
+import (
+	"github.com/bmizerany/assert"
+	"testing"
+	"time"
+)
+
+func TestStore(t *testing.T) {
+	store := NewStore("./test1.db")
+	defer store.Drop()
+
+	item := NewItem("foo")
+	store.Put(item)
+
+	id := item.id
+
+	result, ok := store.Get(id)
+
+	assert.Equal(t, ok, true)
+	assert.Equal(t, result, item)
+
+	store.Remove(id)
+
+	result, ok = store.Get(id)
+
+	assert.Equal(t, ok, false)
+}
+
+func TestStoreConcurrency(t *testing.T) {
+	store := NewStore("./test2.db")
+	defer store.Drop()
+
+	done := make(chan bool)
+
+	put := func() {
+		item := NewItem("foo")
+		store.Put(item)
+		done <- true
+	}
+
+	go put()
+	go put()
+	go put()
+
+	<-done
+	<-done
+	<-done
+
+	assert.Equal(t, store.LastId(), 3)
+}
+
+func TestStoreLoad(t *testing.T) {
+	q := NewQueue("test_store_load")
+
+	item := NewItem("foo")
+	item.queue = q.Name
+
+	temp := NewStore("./test3.db")
+	temp.Put(item)
+	temp.Close()
+
+	store := NewStore("./test3.db")
+	defer store.Drop()
+
+	store.Load()
+
+	_, ok := store.Get(item.id)
+	assert.Equal(t, ok, true)
+
+	ret, _ := q.Dequeue(time.Second, NilDuration)
+	assert.Equal(t, ret.id, item.id)
+}
